dd125b7d7b2e7f1ee23ed9a5d2e54e2dc0bd2fee
tphilly <troy.phillips44@gmail.com>
2024-01-16 17:43:10 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { Link, Routes, Route } from "react-router-dom";
+import Blue from "./components/Blue";
+import Red from "./components/Red";
+import Home from "./components/Home";
+
+function App() {
+  return <><div id="container">
+  <h1>Hello React Router!</h1>
+  <div id="navbar">
+    <Link to= '/blue'>Blue</Link>
+    <Link to= '/red'>Red</Link>
+    <Link to= '/'>Home</Link>
+  </div>
+  <div id="main-section">
+    <Routes>
+      <Route path={"/"} element={<Home/>}/>
+      <Route path={"/blue"} element={<Blue/>}/>
+      <Route path={"/red"} element={<Red/>}/>
+      <Route path={"*"} element={<Home/>}/>
+    </Routes>
+  </div>
+</div></>; 
+}
+
+export default App
